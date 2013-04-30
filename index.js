20d7f9daa88149f1011946d29c1328fcb310e3cb
christian-bromann <contact@christian-bromann.com>
2013-04-30 21:16:10 +0200

    use config.json to setup an enviroment

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ var express    = require("express"),
     path       = require('path'),
     http       = require('http'),
     mongoose   = require('mongoose'),
-    socketio   = require('socket.io');
+    socketio   = require('socket.io'),
+    config     = require('./config.json');
 
 var API = module.exports = function(options) {
 
     'use strict';
 
+    this.options = options;
+
     // create express and server instance
     this.express = express();
     this.server  = http.createServer(this.express);
     // create io instance
-    this.io      = socketio.listen(this.server, { log: false });
+    this.io      = socketio.listen(this.server, { log: this.options.showLogs });
 
 };
 
@@ -26,14 +29,14 @@ var API = module.exports = function(options) {
 API.prototype.setup = function() {
 
     // connect to mongodb
-    mongoose.connect('mongodb://localhost/thEvaluator');
+    this.db = mongoose.createConnection('mongodb://user:pass@host:port/database'.replace(/[a-z]+/g,function(all) { return this[all] || all; }.bind(this.options.db)));
 
     // configure express
     this.express.configure(function(){
         this.express.use(express.bodyParser());
-        this.express.use(express.cookieParser('thEvaluator'));
+        this.express.use(express.cookieParser(this.options.secret));
         this.express.use(express.methodOverride());
-        this.express.use(express.session({secret:'thEvaluator'}));
+        this.express.use(express.session({secret:this.options.secret}));
         this.express.use(this.express.router);
         this.express.use(express.static(path.join(__dirname, "public")));
         this.express.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -49,12 +52,12 @@ API.prototype.start = function() {
 
     this.setup();
 
-    console.log('### app is listen on http://localhost:80 ###');
-    this.server.listen(9001);
+    console.log('### app is listen on http://localhost:%s ###',this.options.port);
+    this.server.listen(this.options.port);
 
 };
 
 // execute start script only if this file was executed
 if(__filename === process.argv[1]) {
-    var app = new API().start();
+    var app = new API(config.enviroments.dev).start();
 }
