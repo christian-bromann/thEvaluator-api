dd6f5a91037bb4f486ff4c477c858309af81439c
christian-bromann <contact@christian-bromann.com>
2013-05-19 12:23:19 +0200

    get testcase by short or long id

--- routes/testcase/get.js
+++ routes/testcase/get.js
@@ -2,8 +2,9 @@ var TestCase = require('../../models/TestCase').model;
 
 exports.type     = 'get';
 exports.url      = '/api/testcase/:id?';
-exports.callback = function(req,res) {
-    var query = req.params.id ? {id:req.params.id} : {};
+exports.callback = function(req,res, next) {
+
+    var query = req.params.id ? req.params.id.length > 10 ? {_id:req.params.id} : {id:req.params.id} : {};
 
     TestCase
         .find(query)
